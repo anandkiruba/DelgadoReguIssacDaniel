7e04b16358a8367a3407040a182e7bdd8934482c
anandkiruba <58231606+anandkiruba@users.noreply.github.com>
2019-11-26 16:17:18 -0500

    updated with working code

--- dao.py
+++ dao.py
@@ -1,1 +1,12 @@
-So we can clone
+import model
+import datetime
+import mysql.connector
+
+cnx = mysql.connector.connect(user='root',password='An@19jesus', host='127.0.0.1', database='pratice_sql')
+cursor = cnx.cursor()
+query_breed=("select id,name,temperament,coat from breed")
+cursor.execute(query_breed)
+for (id,name,temperament,coat) in cursor:
+    print(id,name,temperament,coat)
+cursor.close()
+cnx.close()

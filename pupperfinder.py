b492c830c6712ca8d29ad3fcc55ccb7ae8237f37
anandkiruba <58231606+anandkiruba@users.noreply.github.com>
2019-11-27 12:30:04 -0500

    Update pupperfinder.py

--- pupperfinder.py
+++ pupperfinder.py
@@ -1,1 +1,10 @@
-<placeholder>
+# Pupperfinder.py
+import controller as c
+
+
+def showBreedByID():
+    searchId = int(input("Enter the id you are looking for: "))
+    c.reading(searchId)
+
+
+showBreedByID()

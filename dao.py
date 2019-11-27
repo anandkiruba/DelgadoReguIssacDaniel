d115ff674610d6eec38dd97164e9f548a77d2a78
anandkiruba <58231606+anandkiruba@users.noreply.github.com>
2019-11-27 12:29:39 -0500

    Update dao.py

--- dao.py
+++ dao.py
@@ -1,12 +1,49 @@
-import model
-import datetime
+# Dao.py 1
 import mysql.connector
+import model as m
 
-cnx = mysql.connector.connect(user='root',password='An@19jesus', host='127.0.0.1', database='pratice_sql')
-cursor = cnx.cursor()
-query_breed=("select id,name,temperament,coat from breed")
-cursor.execute(query_breed)
-for (id,name,temperament,coat) in cursor:
-    print(id,name,temperament,coat)
-cursor.close()
-cnx.close()
+
+def connector():
+    cnx = mysql.connector.connect(user='root',
+                                  password='An@19jesus',
+                                  host='127.0.0.1',
+                                  database='pratice_sql')
+
+    return cnx
+
+
+def readByIdBreed(IDs):
+    cnx = connector()
+    mycursor = cnx.cursor()
+    sql = "SELECT * FROM breed WHERE id = %s"
+    idlist = [str(IDs)]
+    mycursor.execute(sql, idlist)
+
+    readData = mycursor.fetchall()
+    breedList = []
+    for item in readData:
+        breedList.append(item)
+
+    for items in breedList:
+        breedObj = m.Breed(items[0], items[1], items[2], items[3])
+        breedObj.display()
+
+    cnx.close()
+
+
+def readByIdPupper():
+    cnx = connector()
+    mycursor = cnx.cursor()
+    sql = "SELECT * FROM pupper"
+    mycursor.execute(sql)
+
+    readData = mycursor.fetchall()
+    breedList = []
+    for item in readData:
+        breedList.append(item)
+
+    for items in breedList:
+        pupperObj = m.Pupper(items[0], items[1], items[2], items[3], items[4], items[5], items[6])
+        pupperObj.display()
+
+    cnx.close()
